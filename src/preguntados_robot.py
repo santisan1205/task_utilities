62d3968b800da616481d3c23e5c6bb92d250bf5d
SinfonIAPepperTeam <sinfonia@uniandes.edu.co>
2025-10-21 13:31:06 -0500

    cambio lenguaje speech

--- src/preguntados_robot.py
+++ src/preguntados_robot.py
@@ -1,21 +1,24 @@
+#!/usr/bin/env python3
 from transitions import Machine
-from task_module import task_module as tm
+from task_module import Task_module as tm
 import threading
-import time
 import rospy
+import time
 import os
 import ConsoleFormatter
 import random
 import json
-class TrivIA(object):
-    """Clase que representa el juego de Preguntados implementado como una máquina de estados.
-    Atributos:
-        consoleFormatter (ConsoleFormatter): Formateador de texto para consola.
-        task_name (str): Nombre del task.
-        tm (task_module): Módulo de tareas para interacción con el robot.
-        qas (dict): Diccionario con preguntas y respuestas.
-    """
+
+class Preguntados(object):
+    
     def __init__(self) -> None:
+        """Clase que representa el juego de Preguntados implementado como una máquina de estados.
+        Atributos:
+            consoleFormatter (ConsoleFormatter): Formateador de texto para consola.
+            task_name (str): Nombre del task.
+            tm (task_module): Módulo de tareas para interacción con el robot.
+            qas (dict): Diccionario con preguntas y respuestas.
+        """
         self.consoleFormatter=ConsoleFormatter.ConsoleFormatter()
         self.task_name = "PREGUNTADOS"
         self.hearing = True
@@ -52,18 +55,18 @@ class TrivIA(object):
     def on_enter_INIT(self):
         print(self.consoleFormatter.format("Inicializacion del task: "+self.task_name, "HEADER"))
         self.tm.initialize_pepper()
-        self.tm.talk("Hola, soy Pepper. Vamos a jugar a Preguntados. Te deseo suerte.")
-        self.tm.talk("El juego consiste en que yo te hare preguntas de diferentes categorias y tu deberas responderlas.")
+        self.tm.talk("Hola, soy Pepper. Vamos a jugar a Preguntados. Te deseo suerte.", language="Spanish")
+        self.tm.talk("El juego consiste en que yo te hare preguntas de diferentes categorias y tu deberas responderlas.", language="Spanish")
         self.begin()
     
     def on_enter_WAIT4GUEST(self):
         print(self.consoleFormatter.format("Esperando a un invitado...", "HEADER"))
-        self.tm.talk("Estoy esperando a que un invitado me diga: Empezar juego, para comenzar una nueva partida.")
+        self.tm.talk("Estoy esperando a que un invitado me diga: Empezar juego, para comenzar una nueva partida.", language="Spanish")
         while self.hearing:
-            text = self.tm.speech2text_srv()
+            text = self.tm.speech2text_srv(seconds=0, lang="esp")
             if text:
                 print(self.consoleFormatter.format("He escuchado: "+text, "OKGREEN"))
-                if "empezar juego" in text.lower():
+                if "jugar" in text.lower():
                     self.new_game = True
                     self.hearing = False
                     self.tm.talk("Perfecto, vamos a empezar una nueva partida.")
@@ -71,29 +74,29 @@ class TrivIA(object):
     
     def on_enter_PREGUNTADOS(self):
         print(self.consoleFormatter.format("Comenzando el juego de Preguntados", "HEADER"))
-        self.tm.talk("Cuantas rondas de preguntas quieres jugar?")
+        self.tm.talk("Cuantas rondas de preguntas quieres jugar?", language="Spanish")
         rounds_text = self.tm.speech2text_srv()
         try:
             self.total_rounds = int(rounds_text)
         except ValueError:
             self.total_rounds = 5
-            self.tm.talk("No he entendido el numero, jugaremos 5 rondas por defecto.")
+            self.tm.talk("No he entendido el numero, jugaremos 5 rondas por defecto.", language="Spanish")
         # Inicializar variables del juego
         self.score = 0
         self.question_number = 1
         self.is_correct = False
         # Empezar el juego
-        self.tm.talk("Muy bien, ya estamos listos para comenzar. Te hare la primera pregunta.")
+        self.tm.talk("Muy bien, ya estamos listos para comenzar. Te hare la primera pregunta.", language="Spanish")
         # Bucle principal del juego, se hacen las preguntas y se verifica la respuesta, esto basado en el numero de rondas provisto
         for _ in range(self.total_rounds):
             self.ask_question()
             if self.is_correct:
-                self.tm.talk("Correcto! Has respondido bien.")
+                self.tm.talk("Correcto! Has respondido bien.", language="Spanish")
                 self.score += 1
             else:
-                self.tm.talk(f"Incorrecto. La respuesta correcta era: {self.current_answers['correct']}.")
-            self.tm.talk(f"Tu puntaje actual es: {self.score}.")
-        self.tm.talk(f"El juego ha terminado. Tu puntaje final es: {self.score} de {self.total_rounds}. Gracias por jugar!")
+                self.tm.talk(f"Incorrecto. La respuesta correcta era: {self.current_answers['correct']}.", language="Spanish")
+            self.tm.talk(f"Tu puntaje actual es: {self.score}.", language="Spanish")
+        self.tm.talk(f"El juego ha terminado. Tu puntaje final es: {self.score} de {self.total_rounds}. Gracias por jugar!", language="Spanish")
         # Se vuelve al estado WAIT4GUEST para esperar un nuevo jugador 
         self.restart()
         
@@ -111,14 +114,14 @@ class TrivIA(object):
         self.tm.talk(
         f"Pregunta {self.question_number}: {pregunta}. "
         f"Las opciones son: {', '.join(respuestas['options'])}."
-        "Por favor, responde con la opcion correcta.")
+        "Por favor, responde con la opcion correcta.", language="Spanish")
         respuesta = self.tm.speech2text_srv()
         # Validar que la respuesta este entre las opciones, en la pantalla muestra si es correcto o no
         if respuesta in self.current_answers["options"]:
             print(self.consoleFormatter.format("He escuchado: "+respuesta, "OKGREEN"))
             self.answer = respuesta
         else:
-            self.tm.talk("No he podido entender tu respuesta. Por favor, intenta de nuevo.")
+            self.tm.talk("No he podido entender tu respuesta. Por favor, intenta de nuevo.", language="Spanish")
             self.answer = None
         # Se verifica si la respuesta es correcta, en patalla se muestra el puntaje actual
         self.is_correct = (self.answer == self.current_answers["correct"])
@@ -129,7 +132,7 @@ class TrivIA(object):
     """Funciones auxiliares"""
     
     def cargar_preguntas(self):
-        with open("preguntas_database.json", "r", encoding="utf-8") as f:
+        with open(os.path.join(os.path.dirname(__file__), "preguntas_database.json"), "r", encoding="utf-8") as f:
             data = json.load(f)
         qas = {}
         for item in data:
@@ -150,3 +153,19 @@ class TrivIA(object):
         for cat, preguntas in qas.items():
             print(f"{cat}: {len(preguntas)} preguntas")
         return qas
+    
+    def check_rospy(self):
+       #Termina todos los procesos al cerrar el nodo
+       while not rospy.is_shutdown():
+           time.sleep(0.1)
+       print(self.consoleFormatter.format("Shutting down", "FAIL"))
+       os._exit(os.EX_OK)
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.start()
+    
+if __name__ == "__main__":
+   sm = Preguntados()
+   sm.run()
+   rospy.spin()
