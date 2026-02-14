a00c5c482b9c67f58efc72d0b1375543069b6125
santisan1205 <santisanchez0512@gmail.com>
2026-02-14 09:04:38 -0500

    avances

--- src/juego_memoria.py
+++ src/juego_memoria.py
@@ -194,4 +194,4 @@ if __name__ == "__main__":
         if juego.state == 'INIT':
             juego.on_enter_INIT()
     threading.Thread(target=kickstart, daemon=True).start()
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8001) 
